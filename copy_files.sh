01279930e1a7658ed7c999a52d2cd980cc217230
drewkeller <954172+drewkeller@users.noreply.github.com>
2026-06-28 13:48:46 -0500

    Use env variable for copying files

--- copy_files.sh
+++ copy_files.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-cp -f DreamFisher.lua DreamFisher.toc Bindings.xml DreamFisher_Tests.lua Requirements.md "UI requirements.md" \
-  "/g/Games/World of Warcraft/_retail_/Interface/AddOns/DreamFisher/"
+ADDON_NAME="DreamFisher"
 
-cp -rf audio buff core fishing ui tests "/g/Games/World of Warcraft/_retail_/Interface/AddOns/DreamFisher/"
+if [[ $WOW_DIR == "" ]]; then
+  echo "Please set the WOW_DIR environment variable."
+  exit 1
+fi
+
+if [[ -d $WOW_DIR ]]; then
+  ADDONS_DIR="$WOW_DIR/_retail_/Interface/AddOns"
+  ADDON_DIR="$ADDONS_DIR/$ADDON_NAME"
+else
+  echo "The directory specified in WOW_DIR does not exist."
+  exit 1
+fi
+
+# Copy the main addon files to the addon directory
+cp -f DreamFisher.lua DreamFisher.toc Bindings.xml DreamFisher_Tests.lua Requirements.md "UI requirements.md" \
+  "$ADDON_DIR"
+
+# Copy the addon subdirectories to the addon directory
+cp -rf audio buff core fishing ui tests "$ADDON_DIR"
